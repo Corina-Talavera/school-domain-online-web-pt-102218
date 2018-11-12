b25e466ffaaca0a683ba106d39e839ad6f330650
Corina Talavera <corina.talavera@gmail.com>
2018-11-12 17:55:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,6 +5,10 @@ class School
     @roster = {}
   end
     def add_student(name, grade)
+      school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
    
     end
   end
