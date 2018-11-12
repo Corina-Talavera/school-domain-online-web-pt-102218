04ec40488dab63f484e767f0e8a9ca5443a185e1
Corina Talavera <corina.talavera@gmail.com>
2018-11-12 17:53:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,22 +1,3 @@
 class School
   attr_accessor :roster
-
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
-  end
-
-  def add_student(name, grade)
-    if @roster[grade] == nil
-      roster[grade] = []
-      @roster[grade] << name
-    else
-      @roster[grade] << name
-    end
-  end
-
-  def grade(grade_level)
-    @roster[grade_level]
-  end
-  school.sort
 end
